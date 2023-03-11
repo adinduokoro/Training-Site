5ed3d15d452d751d1c45ecff291532e7a038f529
adinduokoro <107534645+adinduokoro@users.noreply.github.com>
2023-03-10 22:18:35 -0500

    Navbar
    Basically finished the navbar for desktop and mobile version. Next, need to work on page contents next. The hero section is the next section on the page.

--- src/components/NavbarBottom.js
+++ src/components/NavbarBottom.js
@@ -3,20 +3,51 @@ import "../styles/NavbarBottom.css"
 import Logo from "../images/washington-logo.png"
 import {nav} from "../data.js"
 import { HashLink as Link } from 'react-router-hash-link'
+import { useState } from 'react'
 
 const NavbarBottom = () => {
+
+  const [clicked, setClicked] = useState(false)
+
+  const handleClick = () => {
+    setClicked(!clicked)
+  }
+
+  const closeMenu = () => {
+    setClicked(false)
+  }
+
+
   return (
     <div className='navbarBottom'>
       <div className="nav__logo">
         <img src={Logo} alt="" />
       </div>
-      <i class='bx bx-menu'></i>
+      <div className="menu__icons" onClick={handleClick}>
+        <i className={clicked ? "bx bx-menu" : "bx bx-x"}></i>
+      </div>
+
+      {/* NAVBAR MENU */}
       <ul className="nav__menu">
         {
           nav.map((item, index) =>{
             return(
               <li>
                 <Link smooth to={item.path}><p>{item.text}</p></Link>
+              </li>
+            )
+          })
+        }
+        <button>BOOK NOW</button>
+      </ul>
+
+      {/* MOBILE MENU */}
+      <ul id="mobile__menu" className={clicked ? "mobile__nav active" : "mobile__nav"}>
+        {
+          nav.map((item, index) =>{
+            return(
+              <li>
+                <Link onClick={closeMenu} smooth to={item.path}><p>{item.text}</p></Link>
               </li>
             )
           })
