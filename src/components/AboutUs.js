48832808d4228049d737442fc14adbaa7846ce3b
adinduokoro <107534645+adinduokoro@users.noreply.github.com>
2023-03-15 19:09:39 -0400

    AboutUs
    The about us section is complete. Just need to replace the dummy text.

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -1,11 +1,19 @@
 import React from 'react'
 import '../styles/AboutUs.css'
+import aboutImg from '../images/aboutUs-img.jpg'
 
 const AboutUs = () => {
   return (
     <section className='aboutUs' id='about'>
       <div className="aboutUs__container">
-        hello
+        <img src={aboutImg} alt="" />
+        <div className="overlay"></div>
+        <div className="aboutUs__text">
+          <h2>ABOUT US</h2>
+          <p><span>WASHINGTON'S DIRECT</span> Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis, repellat illum? Vero corporis vitae omnis quia nisi enim quisquam ipsa animi maiores, doloribus, odit aliquid quos ad fuga nostrum at alias esse maxime unde tenetur incidunt amet! Vero perspiciatis qui accusamus nemo soluta inventore eius expedita nihil, corporis facilis quisquam.</p>
+          <br />
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Impedit magnam, deleniti ipsam facere, repellendus culpa corrupti maxime, deserunt et repellat consequatur provident quis. Temporibus, quibusdam..</p>
+        </div>
       </div>
     </section>
   )
