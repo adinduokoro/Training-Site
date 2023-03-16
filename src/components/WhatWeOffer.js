be69bbf7d128dfd9b27f0794f514c36bc8faa0c8
adinduokoro <107534645+adinduokoro@users.noreply.github.com>
2023-03-16 13:19:20 -0400

    WhatWeOffer.
    Imported images to what we offer. Need to style this section.

--- src/components/WhatWeOffer.js
+++ src/components/WhatWeOffer.js
@@ -1,9 +1,28 @@
 import React from 'react'
 import "../styles/WhatWeOffer.css"
+import { service } from '../data'
+
+
 
 const WhatWeOffer = () => {
   return (
-    <section className='whatWeOffer' >WhatWeOffer</section>
+    <section className='whatWeOffer'>
+      <div className="whatWeOffer__container">
+        <h2>WHAT <span>WE OFFER</span></h2>
+        <div className="whatWeOffer__box">
+          <div className="whatWeOffer__card">
+            {service.map((item, index) => {
+              return(
+                <ul key={index}>
+                  <img src={item.img} alt="" />
+                  <p>{item.text}</p>
+                </ul>
+              )
+            })}
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
--- src/data.js
+++ src/data.js
@@ -1,3 +1,10 @@
+import img1 from './images/first-shooter.jpg'
+import img2 from './images/classes-training.png'
+import img3 from './images/self-defense.jpg'
+import img4 from './images/wear-renewal.jpg'
+import img5 from './images/security-guard.jpg'
+import img6 from './images/hql-cert.jpg'
+
 export const nav = [
   {
     text: "HOME",
@@ -21,4 +28,31 @@ export const nav = [
   },
 ]
 
-export default { nav }
+export const service = [
+  {
+    img: img1,
+    text: "FIRST TIME SHOOTER"
+  },
+  {
+    img: img2,
+    text: "CLASSES & TRAINING"
+  },
+  {
+    img: img3,
+    text: "SELF DEFENSE TRAINING"
+  },
+  {
+    img: img4,
+    text: "INITIAL WEAR & CARRY/RENEWAL"
+  },
+  {
+    img: img5,
+    text: "SECURITY GUARD CLEARANCE"
+  },
+  {
+    img: img6,
+    text: "HANDGUN QUALIFICATION CERT. (HQL)"
+  },
+]
+
+export default { nav , service }
