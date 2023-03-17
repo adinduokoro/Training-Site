dba99b41a1ad37ad4241f4e45396050144527dc9
adinduokoro <107534645+adinduokoro@users.noreply.github.com>
2023-03-16 22:05:46 -0400

    Pages.
    Finished adding the majority of the section. Only section missing is the footer.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import AboutUs from "./components/AboutUs";
 import WhatWeOffer from "./components/WhatWeOffer";
 import Classes from "./components/Classes";
 import Location from "./components/Location";
+import Gallery from "./components/Gallery";
+import Contact from "./components/Contact";
 
 function App() {
   return (
@@ -14,6 +16,8 @@ function App() {
       <WhatWeOffer />
       <Classes />
       <Location />
+      <Gallery />
+      <Contact />
     </div>
   );
 }
---
+++ src/components/Contact.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../styles/Contact.css'
+
+const Contact = () => {
+  return (
+    <section className='contact' id='contact'>
+      Contact
+
+    </section>
+  )
+}
+
+export default Contact
