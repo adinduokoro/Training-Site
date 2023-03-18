783b501298064c140675fbe8f0af5aca58737561
adinduokoro <107534645+adinduokoro@users.noreply.github.com>
2023-03-17 23:21:56 -0400

    Gallery
    Working on the image gallery.

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -4,7 +4,12 @@ import '../styles/Gallery.css'
 const Gallery = () => {
   return (
     <section className='gallery'>
-      Gallery
+      <div className="gallery__container">
+        <h2 className='gallerytitle'>Gallery</h2>
+        <div className="gallery__box">
+          pics
+        </div>
+      </div>
     </section>
   )
 }
