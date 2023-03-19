336884862f81454e283b3021cd09bb5d32f64c9d
adinduokoro <107534645+adinduokoro@users.noreply.github.com>
2023-03-18 20:00:04 -0400

    Gallery
    The gallery section is complete.

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,13 +1,21 @@
 import React from 'react'
 import '../styles/Gallery.css'
+import ImageGallery from 'react-image-gallery';
+import "react-image-gallery/styles/css/image-gallery.css"
+import { images } from '../data';
+
 
 const Gallery = () => {
+
   return (
     <section className='gallery'>
       <div className="gallery__container">
-        <h2 className='gallerytitle'>Gallery</h2>
+        <h2 className='gallerytitle'>GALLERY</h2>
         <div className="gallery__box">
-          pics
+          <ImageGallery items={images} 
+            showPlayButton={false}
+            showFullscreenButton={false}
+          />
         </div>
       </div>
     </section>
